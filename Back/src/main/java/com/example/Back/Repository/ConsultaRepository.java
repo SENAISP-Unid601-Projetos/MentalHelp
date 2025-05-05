18b62e1ffd9b20a42bba624e63bb186c884e6a10
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-05-05 16:35:40 -0300

    data para data e hora

--- Back/src/main/java/com/example/Back/Repository/ConsultaRepository.java
+++ Back/src/main/java/com/example/Back/Repository/ConsultaRepository.java
@@ -4,12 +4,13 @@ import com.example.Back.entity.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     Optional<Consulta> findByIdConsulta(Long idConsulta);
 
-    Optional<Object> findByData(LocalDate data);
+    Optional<Object> findByData(LocalDateTime data);
 
     Optional<Object> findByIdProfissional(Long idProfissional);
 
