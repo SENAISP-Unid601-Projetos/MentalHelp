b71238ab4efc71196e9afb47da743a020ebdad24
YuriSantxz07 <yurisantsboy2007@gmail.com>
2025-05-05 15:05:42 -0300

    PacienteDTO e ConsultaDTO

---
+++ Back/src/main/java/com/example/Back/DTO/PacienteDTO.java
@@ -0,0 +1,20 @@
+package com.example.Back.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PacienteDTO {
+    private Long idPaciente;
+    private String nome;
+    private String cpf;
+    private String email;
+
+    private List<Long> id_consultas;
+    private List<Long> id_telefones;
+}
