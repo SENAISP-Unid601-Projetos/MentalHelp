2c2be712c881e29222693cfde3f896e2df27fcf9
pedrocustodio790 <pedrocustodio2006@hotmail.com>
2025-05-05 16:10:02 -0300

    Paciente

--- Back/src/main/java/com/example/Back/Repository/PacienteRepository.java
+++ Back/src/main/java/com/example/Back/Repository/PacienteRepository.java
@@ -1,11 +1,10 @@
 package com.example.Back.Repository;
+
 import com.example.Back.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import java.util.Optional;
 
-public interface PacienteRepository extends JpaRepository <Paciente, Long> {
-    Optional<Paciente> findByIdConsulta(Long idPaciente);
-
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Optional<Paciente> findByEmailAndSenha(String email, String senha);
 }
