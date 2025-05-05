e6bd44d5a9584059f3d934c885d06f974123625a
V1ct0rhugo22 <victorhero53@gmail.com>
2025-05-05 15:29:22 -0300

    Controller Backup

--- Back/src/main/java/com/example/Back/Repository/ProfissionalRepository.java
+++ Back/src/main/java/com/example/Back/Repository/ProfissionalRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface ProfissionalRepository  extends JpaRepository<Profissional, Long> {
 
-   Optional <Profissional> findByIdConsulta(Long idProfissional);
+   Optional <Profissional> findByIdProfissional(Long idProfissional);
 
 }
