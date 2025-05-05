9ddb1d6c3b7c6ea56fcce78f3b4fce989445da7b
pedrocustodio790 <pedrocustodio2006@hotmail.com>
2025-05-05 15:15:45 -0300

    WebConfig

---
+++ Back/src/main/java/com/example/Back/Service/PacienteService.java
@@ -0,0 +1,20 @@
+package com.example.Back.Service;
+
+import com.example.Back.Repository.PacienteRepository;
+import com.example.Back.entity.Paciente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PacienteService {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    Paciente paciente = new Paciente();
+
+
+
+
+
+}
