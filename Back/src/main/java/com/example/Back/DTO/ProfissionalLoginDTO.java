79d22c700f5fda08b8859891847e1f8551e7b94e
YuriSantxz07 <yurisantsboy2007@gmail.com>
2025-05-05 15:16:53 -0300

    LoginDTO update

--- Back/src/main/java/com/example/Back/DTO/ProfissionalLoginDTO.java
+++ Back/src/main/java/com/example/Back/DTO/ProfissionalLoginDTO.java
@@ -1,5 +1,12 @@
 package com.example.Back.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfissionalLoginDTO {
     private String email;
     private String senha;
