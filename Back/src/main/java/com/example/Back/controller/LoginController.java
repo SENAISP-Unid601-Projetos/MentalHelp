f9ce7f70a329493a27e40c0f4389c364c9762ee0
MykyBalb <mykaelbalbino06@gmail.com>
2025-05-21 15:12:44 -0300

    mensagens de erros

--- Back/src/main/java/com/example/Back/controller/LoginController.java
+++ Back/src/main/java/com/example/Back/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.example.Back.Service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
+    @PostMapping
     public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
         String nome = loginService.autenticar(loginRequest.getEmail(), loginRequest.getSenha());
 
